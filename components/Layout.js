4cf9353bd65a5cab49a33d787dea2620f843359f
parth Vakharia <peranoxx@gmail.com>
2023-01-31 16:05:43 +0530

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { useRouter } from 'next/router'
+
+const Layout = ({ children }) => {
+    const router = useRouter()
+    console.log(router.route );
+    if (router.route != '/sign-in' && router.route != '/sign-up') {
+        return <div>
+            <Navbar />
+            <div className="mx-auto max-w-7xl py-12 px-6 lg:px-8">
+                {children}
+            </div>
+            <Footer />
+        </div>
+    }
+    else {
+        return <div>
+            <div className="mx-auto max-w-7xl py-12 px-6 lg:px-8">
+                {children}
+            </div>
+            <Footer />
+        </div>
+    }
+}
+
+export default Layout
